5f133e1a1c39cef96f0a0ee2838d84127ad6ea0e
dan-caldwell <dan@caldwell.info>
2021-12-21 20:57:04 -0500

    basic functionality

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,22 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const axios = require('axios');
+const { parse } = require('node-html-parser');
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const handler = async (req, res) => {
+  let src = null;
+  let title = null;
+  while (!src) {
+    const { data } = await axios.get('https://commons.wikimedia.org/wiki/Special:Random/File');
+    const root = parse(data);
+    const link = root.querySelector('.mw-filepage-other-resolutions a:nth-child(2)');
+    title = root.querySelector('#firstHeading').innerHTML;
+    if (!link) continue;
+    src = link.getAttribute('href');
+  }
+
+  res.status(200).json({
+    src,
+    title
+  })
 }
+
+export default handler;
